af75d905d835aeab1bc8bbdffc30b54e7d510269
Lucas Ribeiro <lucasribeiro.130605@gmail.com>
2023-07-14 23:04:15 -0300

    feat(design): carousel

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,4 @@
-import ReactDOM from 'react-dom/client'
+import * as ReactDOM from 'react-dom/client' // i typed this and this worked: * as
 import App from './App.tsx'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(<App />)
